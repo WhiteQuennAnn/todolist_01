ff48d9c63d02b6dd2f5d0c34354c12fb763541b9
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-06 21:05:27 +0300

    0612022

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,19 @@ function App() {
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false},
     ]
+
+    const todoListTitle_2 = "What to buy";
+    const tasks_2 = [
+        {id: 4, title: "Beer", isDone: true},
+        {id: 5, title: "Cheese", isDone: true},
+        {id: 6, title: "Fish", isDone: false},
+    ]
+
 //GUI
     return (
         <div className="App">
             <Todolist  tasks={tasks} title={todoListTitle}/>
+            <Todolist title={todoListTitle_2} tasks={tasks_2} />
         </div>
 
     );
