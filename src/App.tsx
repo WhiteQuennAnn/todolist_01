89fe003723173f7f38e08ef8fd52b33f3519de55
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-06 20:35:51 +0300

    0612022

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import './App.css';
 import Todolist from "./Todolist";
 
 function App() {
+    const todoListTitle = "What to learn";
+    const tasks = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false}
+    ]
+
     return (
         <div className="App">
            <Todolist />
