dd3103f42b586d56490eca569f523af37f11b910
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-06 17:33:08 +0300

    0612022

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,15 @@
 import React from 'react';
 import './App.css';
+import Todolist from "./Todolist";
 
 function App() {
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+           <Todolist />
+           <Todolist />
+           <Todolist />
+           <Todolist />
+
         </div>
 
     );
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -3,7 +3,21 @@ import React from 'react';
 const Todolist = () => {
     return (
         <div>
-
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
         </div>
     );
 };
