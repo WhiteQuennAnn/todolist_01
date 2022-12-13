8150dbbcf6b90d5b7661e9867f7281502082feed
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-13 19:48:35 +0300

    lesson 6

--- src/components/EditableSpan.tsx
+++ src/components/EditableSpan.tsx
@@ -6,13 +6,15 @@ type EditableSpanPropsType = {
 
 export const EditableSpan = (props: EditableSpanPropsType) => {
     const [edit, setEdit] = useState(true)
+    let [title, setTitle] = useState(props.title)
+
     const changeEdit = () => {
-        setEdit (!edit)
+        setEdit(!edit)
     }
 
     return (
         edit
-            ? <input value={props.title}/>
+            ? <input value={props.title} onBlur={changeEdit} autoFocus/>
             : <span onDoubleClick={changeEdit}>{props.title}</span>
     );
 };
