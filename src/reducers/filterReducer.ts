6e93f99f421029369021806a85cdddff84e18d05
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-21 00:51:17 +0300

    lesson 7

---
+++ src/reducers/filterReducer.ts
@@ -0,0 +1,10 @@
+import {FilterValuesType} from "../App";
+
+export const filterReducer = (state: FilterValuesType, action: any) => {
+    switch (action.type) {
+        case 'xxx': {
+            return state
+        }
+        default: return state
+    }
+}
