0623486c413761edc692b9cff19e68bbd3ca0102
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-21 00:59:29 +0300

    lesson 7

--- src/reducers/filterReducer.ts
+++ src/reducers/filterReducer.ts
@@ -1,18 +1,22 @@
 import {FilterValuesType} from "../App";
 import {removeTaskAC} from "./tasksReducer";
 
-export const filterReducer = (state: FilterValuesType, action: any) => {
+export const filterReducer = (state: FilterValuesType, action: changeFilterACType) => {
     switch (action.type) {
         case 'CHANGE-FILTER': {
-            return state
+            return action.payload.value
         }
-        default: return state
+        default:
+            return state
     }
 }
 type changeFilterACType = ReturnType<typeof changeFilterAC>
 
 export const changeFilterAC = (value: FilterValuesType) => {
     return {
-      type: 'CHANGE-FILTER'
-    }as const
+        type: 'CHANGE-FILTER',
+        payload: {
+            value
+        }
+    } as const
 }
