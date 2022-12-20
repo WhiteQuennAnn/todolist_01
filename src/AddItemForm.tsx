7882b38d8f6658a47493d6ddc3cfa159a7976505
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-20 19:54:54 +0300

    lesson 7

---
+++ src/AddItemForm.tsx
@@ -0,0 +1,46 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import Button from '@mui/material/Button';
+
+type AddItemFormPropsType = {
+    addItem: (title: string) => void
+}
+
+export function AddItemForm(props: AddItemFormPropsType) {
+
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addItem = () => {
+        if (title.trim() !== "") {
+            props.addItem(title);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addItem();
+        }
+    }
+
+    return <div>
+        <input value={title}
+               onChange={onChangeHandler}
+               onKeyPress={onKeyPressHandler}
+               className={error ? "error" : ""}
+        />
+        {/*<button onClick={addItem}>+</button>*/}
+
+        <Button variant="contained" onClick={addItem}
+                style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px', background:'black'}}>+</Button>
+
+        {error && <div className="error-message">{error}</div>}
+    </div>
+}
