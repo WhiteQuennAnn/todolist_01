d7c438324a41464543fc789bf6e7c472628cea46
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-21 00:12:53 +0300

    lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
-    let [tasks, setTasks] = useState([
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "ReactJS", isDone: false},
-        {id: v1(), title: "Rest API", isDone: false},
-        {id: v1(), title: "GraphQL", isDone: false},
-    ]);
+    // let [tasks, setTasks] = useState([
+    //     {id: v1(), title: "HTML&CSS", isDone: true},
+    //     {id: v1(), title: "JS", isDone: true},
+    //     {id: v1(), title: "ReactJS", isDone: false},
+    //     {id: v1(), title: "Rest API", isDone: false},
+    //     {id: v1(), title: "GraphQL", isDone: false},
+    // ]);
 
     let [tasks, tasksDispatch] = useReducer(TasksReducer, [
         {id: v1(), title: "HTML&CSS", isDone: true},
