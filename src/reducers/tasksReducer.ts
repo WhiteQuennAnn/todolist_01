a06b37f82d1eb7a672ee434772e44ee993c0c5f9
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-21 00:27:09 +0300

    lesson 7

--- src/reducers/tasksReducer.ts
+++ src/reducers/tasksReducer.ts
@@ -8,11 +8,13 @@ export const TasksReducer = (state: any, action: any) => {
     }
 }
 
+type removeTaskACType = ReturnType<typeof removeTaskAC>
+
 export const removeTaskAC = (id: string) => {
     return {
         type: " REMOVE-TASK",
         payload: { // коробочка для хлама))
             id: id
         }
-    }
+    } as const
 }
