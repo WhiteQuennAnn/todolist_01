327efed47348883a11186c9ad2ceb079127f8f82
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-27 13:16:53 +0300

    lesson 8

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -17,3 +17,20 @@ test('correct todolist should be removed', () => {
     expect(endState.length).toBe(1)
     expect(endState[0].id).toBe(todolistId2)
 })
+
+test('correct todolist should be added', () => {
+    let todolistId1 = v1()
+    let todolistId2 = v1()
+
+    let newTodolistTitle = 'New Todolist'
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'}
+    ]
+
+    const endState = todolistsReducer(startState, {type: 'ADD-TODOLIST', title: newTodolistTitle})
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(newTodolistTitle)
+})
--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,27 +1,24 @@
 import {TodolistType} from "../App";
 
 export const todolistsReducer = (state: Array<TodolistType>, action: tsarType) => {
-    switch ( action.type) {
+    switch (action.type) {
         case 'REMOVE-TODOLIST' : {
-           // setTodolists(todolists.filter(tl => tl.id != id));
-            // удалим таски для этого тудулиста из второго стейта, где мы храним отдельно таски
-            //delete tasks[id]; // удаляем св-во из объекта... значением которого являлся массив тасок
-            // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-           // setTasks({...tasks});
-            return state.filter (el=> el.id!==action.payload.todolistId1)
+
+            return state.filter(el => el.id !== action.payload.todolistId1)
         }
-        default: return state
+        default:
+            return state
     }
 }
 type tsarType = removeTodolistACType
 
 type removeTodolistACType = ReturnType<typeof removeTodolistAC>
 
-export const removeTodolistAC =(todolistId1: string) => {
+export const removeTodolistAC = (todolistId1: string) => {
     return {
         type: 'REMOVE-TODOLIST',
         payload: {
-           todolistId1
+            todolistId1
         }
-    }as const
+    } as const
 }
