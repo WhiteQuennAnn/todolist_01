db11473de2d0ee65ef0e3b5a711e8d88fa86ed94
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-27 12:39:16 +0300

    lesson 8

---
+++ src/state/user-reducer.test.ts
@@ -0,0 +1,19 @@
+import { userReducer } from './user-reducer'
+
+test.skip('user reducer should increment only age', () => {
+    const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
+
+    const endState = userReducer(startState, {type: 'INCREMENT-AGE'})
+
+    expect(endState.age).toBe(21)
+    expect(endState.childrenCount).toBe(2)
+})
+
+test('user reducer should increment only childrenCount', () => {
+    const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
+
+    const endState = userReducer(startState, {type: 'INCREMENT-CHILDREN-COUNT'})
+
+    expect(endState.age).toBe(21)
+    expect(endState.childrenCount).toBe(2)
+})
