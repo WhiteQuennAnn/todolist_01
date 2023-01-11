8c0c94065b8496b8654e8de021f480d461260f5a
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-01-11 22:19:06 +0300

    lesson 9

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -2,31 +2,21 @@ import {FilterValuesType, TasksStateType, TodolistType} from '../App';
 import {v1} from 'uuid';
 
 export type RemoveTodolistActionType = {
-    type: 'REMOVE-TODOLIST',
-    id: string
+    type: '',
+
 }
 export type AddTodolistActionType = {
-    type: 'ADD-TODOLIST',
-    title: string
+    type: '',
+
 }
-export type ChangeTodolistTitleActionType = {
-    type: 'CHANGE-TODOLIST-TITLE',
-    id: string
-    title: string
-}
-export type ChangeTodolistFilterActionType = {
-    type: 'CHANGE-TODOLIST-FILTER',
-    id: string
-    filter: FilterValuesType
-}
+
 
 type ActionsType =
     RemoveTodolistActionType
     | AddTodolistActionType
-    | ChangeTodolistTitleActionType
-    | ChangeTodolistFilterActionType;
 
-export const tasksReducer = (state: Array<TasksStateType>, action: ActionsType) => {
+
+export const tasksReducer = (state:TasksStateType, action: ActionsType) : TasksStateType => {
     switch (action.type) {
         case '':
             return state
