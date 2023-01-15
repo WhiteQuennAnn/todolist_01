90cbcff67d238373e688ba864765e3da8feb5bb6
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-01-15 18:58:35 +0300

    lesson 9 reducer for tasks, unit test

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -24,17 +24,18 @@ type ActionsType = RemoveTaskActionType | AddTaskActionType
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
         case "REMOVE-TASK": {
-            const stateCopy = {...state}
-            const tasks = state[action.todolistId]
-            const filteredTasks = tasks.filter(t => t.id !== action.taskId)
-            stateCopy[action.todolistId] = filteredTasks
+            const stateCopy = {...state};
+            const tasks = state[action.todolistId];
+            const filteredTasks = tasks.filter(t => t.id !== action.taskId);
+            stateCopy[action.todolistId] = filteredTasks;
             return stateCopy;
         }
         case "ADD-TASK": {
-            const stateCopy = {...state}
-            const tasks = state[action.todolistId]
-            const newTask = {id: v1(), title: action.title, isDone: false}
-            const newTasks = [newTask, ...tasks]
+            const stateCopy = {...state};
+            const tasks = stateCopy[action.todolistId];
+            const newTask = {id: v1(), title: action.title, isDone: false};
+            const newTasks = [newTask, ...tasks];
+            stateCopy[action.todolistId] = newTasks;
             return stateCopy;
         }
         default:
