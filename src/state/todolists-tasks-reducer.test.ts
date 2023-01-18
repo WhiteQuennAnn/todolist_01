3305c18ede02d7990eab34f073a8454e55cf7e87
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-01-18 22:43:13 +0300

    lesson 9 reducer for tasks, unit test

---
+++ src/state/todolists-tasks-reducer.test.ts
@@ -0,0 +1,20 @@
+import {addTodolistAC, todolistsReducer} from "./todolists-reducer";
+import {TasksStateType, TodolistType} from "../App";
+import {tasksReducer} from "./tasks-reducer";
+
+test('ids should be equals', () => {
+    const startTasksState: TasksStateType = {}
+    const startTodolistsState: Array<TodolistType> = []
+
+    const action = addTodolistAC('new todolist')
+
+    const endTasksState = tasksReducer(startTasksState, action)
+    const endTodolistsState = todolistsReducer(startTodolistsState, action)
+
+    const keys = Object.keys(endTasksState)
+    const idFromTasks = keys[0]
+    const idFromTodolists = endTodolistsState[0].id
+
+    expect(idFromTasks).toBe(action.todolistId)
+    expect(idFromTodolists).toBe(action.todolistId)
+})
